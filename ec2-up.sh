24efb444fddbc0aa964c128a7911e768592fa89e
Lalrinfela Pachuau <rinfellapachuau93@gmail.com>
2023-09-13 16:50:03 +0530

    Created EC2 and Lightsail scripts for initiation..

---
+++ ec2-up.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Enter root mode
+sudo su
+
+# Installing OpenLiteSpeed, Certbot and LSPHP
+apt update
+wget -O - https://repo.litespeed.sh | sudo bash
+
+echo "Installing LiteSpeed, LSPHP and certbot.."
+apt update
+apt install openlitespeed certbot lsphp82 lsphp82-{common,curl,mysql}
+
+# Creating www directory
+cd "/usr/local/lsws"
+mkdir "www"
+cd "www"
+echo "www directory created at $PWD"
+
+# Making the project directory and cloning the repo
+
+read -p "Enter the name of your project directory: " dir_name
+
+mkdir   "$dir_name"
+cd      "$dir_name"
+
+read -p "Enter the URL of the project repository: " repo_url
+echo "Cloning the repository.. $repo_url"
+git clone "$repo_url" .
+echo "Repository cloned into $PWD"
+
+# Installing Composer
+echo "Installing Composer.."
+/usr/local/lsws/lsphp81/bin/php8.2 -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+/usr/local/lsws/lsphp81/bin/php8.2 composer-setup.php
+/usr/local/lsws/lsphp81/bin/php8.2 composer.phar install
+
+# Copying the env example file
+cp .env.example .env
+
+# Generating key and linking storage
+/usr/local/lsws/lsphp81/bin/php8.1 artisan key:generate
+/usr/local/lsws/lsphp81/bin/php8.1 artisan storage:link
+
+# Set storage permission to ALL
+chmod -R 777 storage/
+systemctl enable lsws
+systemctl restart lsws
+
+echo "OpenLiteSpeed server configurations has finished."
+echo "Don't forget to configure the .env file manually and set LiteSpeed dashboard admin password."
+echo "You can now start configuring the LiteSpeed dashboard on via the browser.."
+echo "BYE... cya later.."
