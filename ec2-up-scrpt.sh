39cbecc5cf956a66affbf1c1850a13ca48906b39
Lalrinfela Pachuau <rinfellapachuau93@gmail.com>
2023-09-18 10:18:05 +0530

    Removed redundant script file..

--- ec2-up-scrpt.sh
+++
@@ -1,5 +0,0 @@
-# Installing Composer
-echo "Installing Composer.."
-/usr/local/lsws/lsphp81/bin/php8.2 -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-/usr/local/lsws/lsphp81/bin/php8.2 composer-setup.php
-/usr/local/lsws/lsphp81/bin/php8.2 composer.phar install
